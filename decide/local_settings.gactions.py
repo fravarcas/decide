f9218b525218a425b58b6888e949f3177fda3c22
fravarcas <xxraptor51esp@gmail.com>
2022-10-11 17:04:35 +0200

    otra prueba

--- decide/local_settings.gactions.py
+++ decide/local_settings.gactions.py
@@ -6,7 +6,7 @@ DATABASES = {
         'NAME': 'decide',
         'USER': 'decide',
         'PASSWORD': 'decide'
-        'HOST': 'localhost',
+        'HOST': 'localhost:8000',
         'PORT': '5432',
     }
 }
